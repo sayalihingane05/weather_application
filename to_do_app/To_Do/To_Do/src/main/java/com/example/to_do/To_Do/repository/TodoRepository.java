ae42bbf7960e8f62a699f39c6e83aa018320e842
Sayali Bhaskar Hingane <sayalihingane00@gmail.com>
2025-12-17 16:08:48 +0530

    Delete to_do_app directory

--- to_do_app/To_Do/To_Do/src/main/java/com/example/to_do/To_Do/repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.to_do.To_Do.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.to_do.To_Do.model.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {}
